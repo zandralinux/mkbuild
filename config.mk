1cd0eb9eb568e6757335930e64a68c914f251d12
Daniel Bainton <dpb@driftaway.org>
2014-04-07 13:41:00 +0300

    Remove some morpheus specific stuff

--- config.mk
+++ config.mk
@@ -1,7 +1,4 @@
 arch = i486
-version = 0.0
-mirror = http://dl.2f30.org/morpheus-pkgs/${arch}/${version}
-ports = $mkbuild/../ports
 nprocs = 2
 
 TOOLCHAIN_TRIPLET = ${arch}-musl-linux
@@ -23,10 +20,6 @@ MANDIR = ${PREFIX}/share/man
 
 INSTALL = /usr/bin/install
 
-# these don't work for all packages yet...
-optldflags = -s -Wl,--gc-sections -Wl,-z,relro,-z,now
-optcflags = -fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack
-
 CPPFLAGS = -D_BSD_SOURCE -D_GNU_SOURCE
-CFLAGS = ${CPPFLAGS} # ${optcflags}
-LDFLAGS = -static ${optldflags}
+CFLAGS = ${CPPFLAGS}
+LDFLAGS = -static
